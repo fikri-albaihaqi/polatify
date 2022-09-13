2e46f2ceadf207068ee02b945eb72d044847c140
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2022-09-13 22:56:26 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, './public/')));
 
   // Handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + './public/index.html'));
+  app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 }
 
 const generateRandomString = function (length) {
