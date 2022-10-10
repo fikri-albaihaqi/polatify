138ad3595269e7d9f74256fb8cdc50ca0959726f
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2022-10-10 20:51:51 +0700

    Added Spotify logo

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -5,7 +5,9 @@ export default {
 </script>
 
 <template>
-  <footer class="mt-auto h-12 text-center">
+  <footer class="flex flex-col items-center mt-auto h-12 text-center">
     <h2>Created with ❤ by Fikri © 2022</h2>
+    <h2>Powered by</h2>
+    <img class="w-[160px] m-[23.975px] pb-[23.975px]" src="../assets/Spotify_Logo_RGB_Black.png" alt="">
   </footer>
 </template>
