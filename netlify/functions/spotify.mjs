9736ca22e2e65e6eabf76bad6348782975fdca87
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2024-08-06 20:59:17 +0700

    feat: setup for netlify deploy

--- netlify/functions/spotify.mjs
+++ netlify/functions/spotify.mjs
@@ -1,51 +1,51 @@
-require('dotenv').config();
+import express, { Router } from "express"
+import serverless from "serverless-http"
+import querystring from "query-string"
+import cors from "cors"
+import path from "path"
+import request from "request"
 
-const express = require('express');
-const querystring = require('querystring');
-const cors = require('cors');
-const path = require('path');
-const request = require('request');
+const api = express()
+const router = Router()
 
-const PORT = process.env.PORT || 8888;
-const client_id = process.env.CLIENT_ID;
-const client_secret = process.env.CLIENT_SECRET;
-let redirect_uri = process.env.REDIRECT_URI || 'http://localhost:8888/callback';
-let frontend_uri = process.env.FRONTEND_URI || 'http://localhost:5173';
+const PORT = process.env.PORT || 8888
+const client_id = process.env.CLIENT_ID
+const client_secret = process.env.CLIENT_SECRET
+let redirect_uri = process.env.REDIRECT_URI || 'http://localhost:5173/callback'
+let frontend_uri = process.env.FRONTEND_URI || 'http://localhost:5173'
 
 if (process.env.NODE_ENV !== 'production') {
-  redirect_uri = 'http://localhost:8888/callback';
-  frontend_uri = 'http://localhost:5173';
+  redirect_uri = 'http://localhost:5173/callback'
+  frontend_uri = 'http://localhost:5173'
 }
 
-const app = express();
-
 if (process.env.NODE_ENV !== 'production') {
-  app.use(express.static(path.resolve(__dirname, '../client'))).use(cors());
+  api.use(express.static(path.resolve(__dirname, '../client'))).use(cors())
 }
 
 // Handle prdouction
 if (process.env.NODE_ENV === 'production') {
   // Static folder
-  app.use(express.static(path.resolve(__dirname + '/public/')));
+  api.use(express.static(path.resolve(__dirname + '/public/')))
 
   // Handle SPA
-  app.get('/', (req, res) => res.sendFile(path.resolve(__dirname + '/public/index.html')));
+  api.get('/', (req, res) => res.sendFile(path.resolve(__dirname + '/public/index.html')))
 }
 
 const generateRandomString = function (length) {
-  let text = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let text = ''
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
   for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
   }
-  return text;
-};
+  return text
+}
 
-app.get('/login', function (req, res) {
+router.get('/login', function (req, res) {
 
-  let state = generateRandomString(16);
-  const scope = 'user-read-email user-top-read user-follow-read playlist-read-private playlist-read-collaborative';
+  let state = generateRandomString(16)
+  const scope = 'user-read-email user-top-read user-follow-read playlist-read-private playlist-read-collaborative'
 
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
@@ -54,19 +54,19 @@ app.get('/login', function (req, res) {
       scope: scope,
       redirect_uri: redirect_uri,
       state: state
-    }));
-});
+    }))
+})
 
-app.get('/callback', function (req, res) {
+router.get('/callback', function (req, res) {
 
-  let code = req.query.code || null;
-  let state = req.query.state || null;
+  let code = req.query.code || null
+  let state = req.query.state || null
 
   if (state === null) {
     res.redirect('/#' +
       querystring.stringify({
         error: 'state_mismatch'
-      }));
+      }))
   } else {
     const authOptions = {
       url: 'https://accounts.spotify.com/api/token',
@@ -79,13 +79,13 @@ app.get('/callback', function (req, res) {
         'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
       },
       json: true
-    };
+    }
 
     request.post(authOptions, function (error, response, body) {
       if (!error && response.statusCode === 200) {
 
         const access_token = body.access_token,
-          refresh_token = body.refresh_token;
+          refresh_token = body.refresh_token
 
         // we can also pass the token to the browser to make requests from there
         res.redirect(
@@ -93,17 +93,17 @@ app.get('/callback', function (req, res) {
             access_token: access_token,
             refresh_token: refresh_token,
           })}`
-        );
+        )
       } else {
-        res.redirect(`/#${querystring.stringify({ error: 'invalid_token' })}`);
+        res.redirect(`/#${querystring.stringify({ error: 'invalid_token' })}`)
       }
-    });
+    })
   }
-});
+})
 
-app.get('/refresh_token', function (req, res) {
+router.get('/refresh_token', function (req, res) {
 
-  const refresh_token = req.query.refresh_token;
+  const refresh_token = req.query.refresh_token
   const authOptions = {
     url: 'https://accounts.spotify.com/api/token',
     headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
@@ -112,18 +112,22 @@ app.get('/refresh_token', function (req, res) {
       refresh_token: refresh_token
     },
     json: true
-  };
+  }
 
   request.post(authOptions, function (error, response, body) {
     if (!error && response.statusCode === 200) {
-      const access_token = body.access_token;
+      const access_token = body.access_token
       res.send({
         'access_token': access_token
-      });
+      })
     }
-  });
-});
+  })
+})
 
-app.listen(PORT, function () {
-  console.warn(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
-});
+api.listen(PORT, function () {
+  console.warn(`Node cluster worker ${process.pid}: listening on port ${PORT}`)
+})
+
+api.use("/spotify/", router)
+
+export const handler = serverless(api)
